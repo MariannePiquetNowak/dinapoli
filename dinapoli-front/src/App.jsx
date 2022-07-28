b999e74be1898946de6cc7da0835dc7313c69185
Marianne Piquet <mpiquet@terresens.com>
2022-07-28 15:57:53 +0200

    card header on all views

--- dinapoli-front/src/App.jsx
+++ dinapoli-front/src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 // Views (Pages)
-import Home from './Views/Home/Home';
-import Blog from './Views/Blog/Blog';
-import Team from './Views/Team/Team';
+import Home from "./Views/Home/Home";
+import Blog from "./Views/Blog/Blog";
+import Team from "./Views/Team/Team";
 
 // Components
-import Cardy from './Components/Card/Card';
-import img from "./assets/images/image-1.png"
+import Cardy from "./Components/Card/Card";
+import img from "./assets/images/image-1.png";
 
 // Layouts
 import Header from "./Layouts/Header/Header";
@@ -17,25 +17,78 @@ export default function App() {
   return (
     <>
       <Header />
-      {window.location.pathname === "/" ? 
-        <Cardy
-            otherClass="header-card"
-            reverse="reverse" 
-            title="Le Savoir-faire, un état d'esprit" 
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros augue, dapibus ac quam non, fringilla placerat ex." 
-            img={img} 
-            link={false}
-            href=""
-            linkText=""
-            imgOk={true}
-        />
-      : null}
+      <Card />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/blog' element={<Blog />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
         <Route path="/team" element={<Team />} />
       </Routes>
       <Footer />
     </>
-  )
+  );
 }
+
+const Card = () => {
+  const pathname = window.location.pathname;
+
+  switch (pathname) {
+    case "/":
+      return (
+        <Cardy
+          otherClass="header-card"
+          reverse="reverse"
+          title="Le Savoir-faire, un état d'esprit"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros augue, dapibus ac quam non, fringilla placerat ex."
+          img={img}
+          link={false}
+          href=""
+          linkText=""
+          imgOk={true}
+        />
+      );
+    case "/team":
+      return (
+        <Cardy
+          otherClass="header-card"
+          reverse="reverse"
+          title="Et si on parlait de Nous ?"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros augue, dapibus ac quam non, fringilla placerat ex. In eros augue, dapibus ac quam non, fringilla placerat ex."
+          img={img}
+          link={false}
+          href=""
+          linkText=""
+          imgOk={true}
+        />
+      );
+    case "/gallery":
+      return (
+        <Cardy
+          otherClass="header-card"
+          reverse="reverse"
+          title="Quand la Squadra s'amuse"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros augue, dapibus ac quam non, fringilla placerat ex."
+          img={img}
+          link={false}
+          href=""
+          linkText=""
+          imgOk={true}
+        />
+      );
+      case "/products":
+      return (
+        <Cardy
+          otherClass="header-card"
+          reverse="reverse"
+          title="Nos produits, un respect pour la pizza"
+          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros augue, dapibus ac quam non, fringilla placerat ex."
+          img={img}
+          link={false}
+          href=""
+          linkText=""
+          imgOk={true}
+        />
+      );
+    default:
+      return;
+  }
+};
--- dinapoli-front/src/Views/Team/Team.jsx
+++ dinapoli-front/src/Views/Team/Team.jsx
@@ -1,8 +1,27 @@
-import React, { usetState, useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
+import Card from '../../Components/Card/Card';
+import img from '../../assets/images/image-1.png';
 
 const Team = () => {
+
+    const [members, setMember] = useState();
+
+    useEffect(() => {
+
+    }, []);
+
     return (
-        <>Coucou team</>
+        <main className="Team mt-3">
+            {/* <Card
+                reverse="reverse" 
+                title="Un peu d'histoire sur la Pizza" 
+                text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros augue, dapibus ac quam non, fringilla placerat ex." 
+                img={img} 
+                link={true}
+                href="/blog"
+                hrefText="Voir notre blog"
+            /> */}
+        </main>
     )
 }
 
