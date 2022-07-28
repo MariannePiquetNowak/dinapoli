9939550d886a1899b7eb79bc6fd11ce4469ae3a4
Marianne Piquet <mpiquet@terresens.com>
2022-07-28 11:41:07 +0200

    add bg img blogAccess & reverse card

--- dinapoli-front/src/Layouts/BlogAccess/Blog.jsx
+++ dinapoli-front/src/Layouts/BlogAccess/Blog.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Cardy from "../../Components/Card/Card";
-import img from "../../assets/images/image-1.png";
+import img from "../../assets/images/pate.png";
 
 const BlogAccess = () => {
    
     return (
         <section className="blog-access">
             <Cardy
-                reverse="" 
+                reverse="reverse" 
                 title="Un peu d'histoire sur la Pizza" 
                 text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eros augue, dapibus ac quam non, fringilla placerat ex." 
                 img={img} 
